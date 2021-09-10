556d4a9ac4bf70276ce70802c8fc4f70cf76088a
Nadya <snotra.notra@gmail.com>
2021-09-10 19:31:02 +0300

    feat: resolve st-delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,7 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+export default function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = 0;
+  let i = 1;
+  while (Math.trunc(n / i) > 0) {
+    let temp = Math.trunc(n / (i * 10)) * i + (n % i);
+    i *= 10;
+    result = Math.max(result, temp);
+  }
+  return result;
 }
